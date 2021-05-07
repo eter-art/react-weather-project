f89de042e6746e2e8eddeb044e935a579001d787
eter-art <77591625+eter-art@users.noreply.github.com>
2021-05-07 11:20:11 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">  <h1>Weather App</h1>
+    <div className="App"> 
+    <div className="container">
+     <h1>Weather App</h1>
     <footer>
    <a href="https://github.com/eter-art/react-weather-project"   target="_blank" rel="noreferrer">   Open source code   </a>  
 by Eter Artmeladze
     </footer>
+    </div>
     </div>
   );
 }
