75f46c795dd93282e67df5fc39167eae9edff8d2
eter-art <77591625+eter-art@users.noreply.github.com>
2021-05-07 13:56:21 +0200

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -7,22 +7,29 @@ export default function Weather (){
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder= "enter a city..." className="form-control" />
+                <input type="search" placeholder= "Enter a city..." className="form-control" autoFocus="on" />
                 </div>
                 <div className="col-3">
-                <input type="Submit" value="Search" className="btn btn-primary"/>
+                <input type="Submit" value="Search" className="btn btn-primary w-100"/>
             </div>
             </div>
             </form>
             <h1>Tbilisi</h1>
             <ul>
                 <li> friday 13:00 </li>
-                <li>Sunny </li>
+                <li>Mostly Cloudy </li>
                </ul>
-               <div className="row">
+                     <div className="row">
                    <div className="col-6">
-                       <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly sunny"/>
-                       21°C
+                       <div className="clearfix">
+                       <img
+                        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                         alt="Mostly Sunny"  />  
+                           
+                       
+                       <span className="temperature">21</span>
+                       <span className= "unit">°C</span> 
+                 </div>
                    </div>
                    <div className="col-6">
                        <ul>
@@ -36,6 +43,7 @@ export default function Weather (){
                                 Wind: 40 km/h
                             </li>
                        </ul>
+                       
                    </div>
                </div>
             </div>
