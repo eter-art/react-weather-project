f9110b5e9345cb390c0f92c2fa7c43fbcbb5c4c5
eter-art <77591625+eter-art@users.noreply.github.com>
2021-05-10 10:44:11 +0200

    added formatted date

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App"> 
     <div className="container">
-     <Weather/>
+     <Weather defaultCity="Tbilisi"/>
     <footer>
    <a href="https://github.com/eter-art/react-weather-project"   target="_blank" rel="noreferrer">   Open source code   </a>  
 by Eter Artmeladze
